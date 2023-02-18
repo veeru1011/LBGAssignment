7706afa15f81af5d9a24e526e948703dc3ad17b0
veer bahadur tiwari <veeru.ramjas@gmail.com>
2023-02-19 01:42:54 +0530

    Initial Commit with Basci structure

---
+++ LBGAssignment/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LBGAssignment
+//
+//  Created by mac on 16/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LBGAssignment/Utilities/Constants/APIConstants.swift
@@ -0,0 +1,15 @@
+//
+//  APIConstants.swift
+//  LBGAssignment
+//
+//  Created by mac on 16/02/23.
+//
+
+import Foundation
+
+struct APIConstants {
+    
+    static let apiURL = "https://api.seatgeek.com/2/"
+    
+    static let clientID = "MjI4NzYyNDV8MTYyODc1OTQzNi41NzU2MjU"
+}
